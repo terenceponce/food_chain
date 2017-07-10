2d533a30da7025b7b8afd579fd35cb8d5a2cf5cc
Terence Ponce <terence.ponce@promisepay.com>
2017-07-11 00:02:20 +0800

    Refactor song generation

--- food_chain.rb
+++ food_chain.rb
@@ -1,101 +1,143 @@
 class FoodChain
   def self.song
-    song = ""
-    song += know_old_lady_swallowed('fly')
-    song += dont_know_why_swallowed
+    animals = ['Fly', 'Spider', 'Bird', 'Cat', 'Dog', 'Goat', 'Cow', 'Horse']
+    song = ''
 
-    song += know_old_lady_swallowed('spider')
-    song += animal_action('spider')
-    song += swallow_to_catch('spider')
-    song += dont_know_why_swallowed
+    animals.each do |animal|
+      song += Object.const_get(animal).new.send(:verse)
+    end
 
-    song += know_old_lady_swallowed('bird')
-    song += animal_action('bird')
-    song += swallow_to_catch('bird')
-    song += swallow_to_catch('spider')
-    song += dont_know_why_swallowed
+    song
+  end
+end
 
-    song += know_old_lady_swallowed('cat')
-    song += animal_action('cat')
-    song += swallow_to_catch('cat')
-    song += swallow_to_catch('bird')
-    song += swallow_to_catch('spider')
-    song += dont_know_why_swallowed
-
-    song += know_old_lady_swallowed('dog')
-    song += animal_action('dog')
-    song += swallow_to_catch('dog')
-    song += swallow_to_catch('cat')
-    song += swallow_to_catch('bird')
-    song += swallow_to_catch('spider')
-    song += dont_know_why_swallowed
-
-    song += know_old_lady_swallowed('goat')
-    song += animal_action('goat')
-    song += swallow_to_catch('goat')
-    song += swallow_to_catch('dog')
-    song += swallow_to_catch('cat')
-    song += swallow_to_catch('bird')
-    song += swallow_to_catch('spider')
-    song += dont_know_why_swallowed
-
-    song += know_old_lady_swallowed('cow')
-    song += animal_action('cow')
-    song += swallow_to_catch('cow')
-    song += swallow_to_catch('goat')
-    song += swallow_to_catch('dog')
-    song += swallow_to_catch('cat')
-    song += swallow_to_catch('bird')
-    song += swallow_to_catch('spider')
-    song += dont_know_why_swallowed
-
-    song += know_old_lady_swallowed('horse')
-    song += animal_action('horse')
-
-    return song
+class Animal
+  def verse
+    output = ""
+    output += know_old_lady_swallowed
+    output += action
+    output += swallow_to_catch
+    output += dont_know_why_swallowed
+    return output
   end
 
-  def self.know_old_lady_swallowed(animal)
-    "I know an old lady who swallowed a #{animal}.\n"
+  def prey
+    ''
   end
 
-  def self.dont_know_why_swallowed
+  def know_old_lady_swallowed
+    "I know an old lady who swallowed a #{self.class.name.downcase}.\n"
+  end
+
+  def dont_know_why_swallowed
     "I don't know why she swallowed the fly. Perhaps she'll die.\n\n"
   end
 
-  def self.animal_action(animal)
-    case animal
-    when 'spider'
-      "It wriggled and jiggled and tickled inside her.\n"
-    when 'bird'
-      "How absurd to swallow a bird!\n"
-    when 'cat'
-      "Imagine that, to swallow a cat!\n"
-    when 'dog'
-      "What a hog, to swallow a dog!\n"
-    when 'goat'
-      "Just opened her throat and swallowed a goat!\n"
-    when 'cow'
-      "I don't know how she swallowed a cow!\n"
-    when 'horse'
-      "She's dead, of course!\n"
-    end
+  def action
+    ''
   end
 
-  def self.swallow_to_catch(animal)
-    case animal
-    when 'spider'
-      "She swallowed the spider to catch the fly.\n"
-    when 'bird'
-      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n"
-    when 'cat'
-      "She swallowed the cat to catch the bird.\n"
-    when 'dog'
-      "She swallowed the dog to catch the cat.\n"
-    when 'goat'
-      "She swallowed the goat to catch the dog.\n"
-    when 'cow'
-      "She swallowed the cow to catch the goat.\n"
-    end
+  def swallow_to_catch
+    output = ""
+    output += swallow_phrase unless swallow_phrase.empty?
+    output += Object.const_get(prey).new.send(:swallow_to_catch) unless prey.empty?
+    output
+  end
+
+  def swallow_phrase
+    ''
+  end
+end
+
+class Fly < Animal
+end
+
+class Spider < Animal
+  def action
+    "It wriggled and jiggled and tickled inside her.\n"
+  end
+
+  def swallow_phrase
+    "She swallowed the spider to catch the fly.\n"
+  end
+end
+
+class Bird < Animal
+  def prey
+    'Spider'
+  end
+
+  def action
+    "How absurd to swallow a bird!\n"
+  end
+
+  def swallow_phrase
+    "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n"
+  end
+end
+
+class Cat < Animal
+  def prey
+    'Bird'
+  end
+
+  def action
+    "Imagine that, to swallow a cat!\n"
+  end
+
+  def swallow_phrase
+    "She swallowed the cat to catch the bird.\n"
+  end
+end
+
+class Dog < Animal
+  def prey
+    'Cat'
+  end
+
+  def action
+    "What a hog, to swallow a dog!\n"
+  end
+
+  def swallow_phrase
+    "She swallowed the dog to catch the cat.\n"
+  end
+end
+
+class Goat < Animal
+  def prey
+    'Dog'
+  end
+
+  def action
+    "Just opened her throat and swallowed a goat!\n"
+  end
+
+  def swallow_phrase
+    "She swallowed the goat to catch the dog.\n"
+  end
+end
+
+class Cow < Animal
+  def prey
+    'Goat'
+  end
+
+  def action
+    "I don't know how she swallowed a cow!\n"
+  end
+
+  def swallow_phrase
+    "She swallowed the cow to catch the goat.\n"
+  end
+end
+
+class Horse < Animal
+  def action
+    "She's dead, of course!\n"
+  end
+
+  def dont_know_why_swallowed
+    ''
   end
 end
--- food_chain_test.rb
+++ food_chain_test.rb
@@ -50,130 +50,130 @@ class FoodChainTest < Minitest::Test
     assert_equal 2, BookKeeping::VERSION
   end
 
-  ###########################################
-  # FoodChain.know_old_lady_swallowed tests #
-  ###########################################
+  ########################################
+  # Animal.know_old_lady_swallowed tests #
+  ########################################
 
   def test_know_old_lady_swallowed_fly
     expected = "I know an old lady who swallowed a fly.\n"
-    assert_equal expected, FoodChain.know_old_lady_swallowed('fly')
+    assert_equal expected, Fly.new.know_old_lady_swallowed
   end
 
   def test_know_old_lady_swallowed_spider
     expected = "I know an old lady who swallowed a spider.\n"
-    assert_equal expected, FoodChain.know_old_lady_swallowed('spider')
+    assert_equal expected, Spider.new.know_old_lady_swallowed
   end
 
   def test_know_old_lady_swallowed_bird
     expected = "I know an old lady who swallowed a bird.\n"
-    assert_equal expected, FoodChain.know_old_lady_swallowed('bird')
+    assert_equal expected, Bird.new.know_old_lady_swallowed
   end
 
   def test_know_old_lady_swallowed_cat
     expected = "I know an old lady who swallowed a cat.\n"
-    assert_equal expected, FoodChain.know_old_lady_swallowed('cat')
+    assert_equal expected, Cat.new.know_old_lady_swallowed
   end
 
   def test_know_old_lady_swallowed_dog
     expected = "I know an old lady who swallowed a dog.\n"
-    assert_equal expected, FoodChain.know_old_lady_swallowed('dog')
+    assert_equal expected, Dog.new.know_old_lady_swallowed
   end
 
   def test_know_old_lady_swallowed_goat
     expected = "I know an old lady who swallowed a goat.\n"
-    assert_equal expected, FoodChain.know_old_lady_swallowed('goat')
+    assert_equal expected, Goat.new.know_old_lady_swallowed
   end
 
   def test_know_old_lady_swallowed_cow
     expected = "I know an old lady who swallowed a cow.\n"
-    assert_equal expected, FoodChain.know_old_lady_swallowed('cow')
+    assert_equal expected, Cow.new.know_old_lady_swallowed
   end
 
   def test_know_old_lady_swallowed_horse
     expected = "I know an old lady who swallowed a horse.\n"
-    assert_equal expected, FoodChain.know_old_lady_swallowed('horse')
+    assert_equal expected, Horse.new.know_old_lady_swallowed
   end
 
-  ##########################################
-  # FoodChain.dont_know_why_swallowed test #
-  ##########################################
+  #######################################
+  # Animal.dont_know_why_swallowed test #
+  #######################################
 
   def test_dont_know_why_swallowed
     expected = "I don't know why she swallowed the fly. Perhaps she'll die.\n\n"
-    assert_equal expected, FoodChain.dont_know_why_swallowed
+    assert_equal expected, Animal.new.dont_know_why_swallowed
   end
 
-  #################################
-  # FoodChain.animal_action tests #
-  #################################
+  #######################
+  # Animal.action tests #
+  #######################
 
   def test_animal_action_spider
     expected = "It wriggled and jiggled and tickled inside her.\n"
-    assert_equal expected, FoodChain.animal_action('spider')
+    assert_equal expected, Spider.new.action
   end
 
   def test_animal_action_bird
     expected = "How absurd to swallow a bird!\n"
-    assert_equal expected, FoodChain.animal_action('bird')
+    assert_equal expected, Bird.new.action
   end
 
   def test_animal_action_cat
     expected = "Imagine that, to swallow a cat!\n"
-    assert_equal expected, FoodChain.animal_action('cat')
+    assert_equal expected, Cat.new.action
   end
 
   def test_animal_action_dog
     expected = "What a hog, to swallow a dog!\n"
-    assert_equal expected, FoodChain.animal_action('dog')
+    assert_equal expected, Dog.new.action
   end
 
   def test_animal_action_goat
     expected = "Just opened her throat and swallowed a goat!\n"
-    assert_equal expected, FoodChain.animal_action('goat')
+    assert_equal expected, Goat.new.action
   end
 
   def test_animal_action_cow
     expected = "I don't know how she swallowed a cow!\n"
-    assert_equal expected, FoodChain.animal_action('cow')
+    assert_equal expected, Cow.new.action
   end
 
   def test_animal_action_horse
     expected = "She's dead, of course!\n"
-    assert_equal expected, FoodChain.animal_action('horse')
+    assert_equal expected, Horse.new.action
   end
 
-  ####################################
-  # FoodChain.swallow_to_catch tests #
-  ####################################
+  #################################
+  # Animal.swallow_to_catch tests #
+  #################################
 
   def test_swallow_to_catch_spider
     expected = "She swallowed the spider to catch the fly.\n"
-    assert_equal expected, FoodChain.swallow_to_catch('spider')
+    assert_equal expected, Spider.new.swallow_to_catch
   end
 
   def test_swallow_to_catch_bird
-    expected = "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n"
-    assert_equal expected, FoodChain.swallow_to_catch('bird')
+    expected = "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\n"
+    assert_equal expected, Bird.new.swallow_to_catch
   end
 
   def test_swallow_to_catch_cat
-    expected = "She swallowed the cat to catch the bird.\n"
-    assert_equal expected, FoodChain.swallow_to_catch('cat')
+    expected = "She swallowed the cat to catch the bird.\nShe swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\n"
+    assert_equal expected, Cat.new.swallow_to_catch
   end
 
   def test_swallow_to_catch_dog
-    expected = "She swallowed the dog to catch the cat.\n"
-    assert_equal expected, FoodChain.swallow_to_catch('dog')
+    expected = "She swallowed the dog to catch the cat.\nShe swallowed the cat to catch the bird.\nShe swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\n"
+    assert_equal expected, Dog.new.swallow_to_catch
   end
 
   def test_swallow_to_catch_goat
-    expected = "She swallowed the goat to catch the dog.\n"
-    assert_equal expected, FoodChain.swallow_to_catch('goat')
+    expected = "She swallowed the goat to catch the dog.\nShe swallowed the dog to catch the cat.\nShe swallowed the cat to catch the bird.\nShe swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\n"
+    assert_equal expected, Goat.new.swallow_to_catch
   end
 
   def test_swallow_to_catch_cow
-    expected = "She swallowed the cow to catch the goat.\n"
-    assert_equal expected, FoodChain.swallow_to_catch('cow')
+    expected = "She swallowed the cow to catch the goat.\nShe swallowed the goat to catch the dog.\nShe swallowed the dog to catch the cat.\nShe swallowed the cat to catch the bird.\nShe swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\n"
+    assert_equal expected, Cow.new.swallow_to_catch
   end
 end
 
